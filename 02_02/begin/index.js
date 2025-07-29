cf3846c64eca143adde9c407f63804504a85e3b7
rdionis <dionisio.raquel@gmail.com>
2025-07-29 16:31:25 +0200

    http methods

--- 02_02/begin/index.js
+++ 02_02/begin/index.js
@@ -1,6 +1,42 @@
 import express from 'express';
+import data from "./data/mock.json" with { type: "json" };
 
 const app = express();
+
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+// GET
+app.get("/", (request, response) => {
+  response.send("This is a GET request at /.");
+});
+
+// POST
+app.post("/create", (request, response) => {
+  response.send("This is a POST request at /create.");
+});
+
+// PUT
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit.");
+});
+
+// DELETE
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete.");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`)
+  console.log(data);
+});
